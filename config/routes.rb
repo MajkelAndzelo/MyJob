9810c61636272bbf14c8bfab1457f7f2cb072c8d
Mikel Andzelo <mdybka03@gmail.com>
2023-03-04 03:32:59 +0100

    bug fixes

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+  get "logout", to: "pages#home"
   get "password/reset", to: "password_resets#new"
   post "password/reset", to: "password_resets#create"
   get "password/reset/edit", to: "password_resets#edit"
